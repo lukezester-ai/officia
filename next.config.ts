821d2ea425c64607835212d655890d4035f27955
Твоето име <lukezester@gmail.com>
2026-06-21 22:12:17 +0300

    feat(core): convert web app to progressive web app (pwa)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from 'next';
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 
 const nextConfig: NextConfig = {
   // strict types and linting for production
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
